1e8a7f6573b2d88528bbab762a0ab8c68d3f25e3
Hiroya Furusawa <pogbba.6@gmail.com>
2023-01-15 20:58:00 +0900

    巻き上げとクロージャーについてのセクションを追加

--- src/section_function_scope/index.js
+++ src/section_function_scope/index.js
@@ -96,3 +96,117 @@ function fn() {
   console.log(x4); // => "varのx"
 }
 fn();
+
+
+/*
+* 関数宣言と巻き上げ
+* ・function キーワードによる関数宣言も関数が実行された時点で、近い関数、またはグローバルスコープの先頭に巻き上げられる */
+  // testFn() の宣言が巻き上げられている
+  testFn(); // "testFn"
+  function testFn() {
+    return console.log("testFn");
+  }
+
+
+/*
+* クロージャー
+*/
+  /* 静的スコープ
+   ・JavaScript はどの識別子がその変数を参照するかコードを実行する前にわかっている。*/
+    const x7 = 10; // *1
+
+    function printX7() {
+      console.log(x7); // 10
+    }
+    function run() {
+      const x7 = 20; // *2
+      printX7();
+    }
+    run();
+    // printX7() の x7 は以下の順番で名前解決されている
+    // ① printX7 の関数内に x7 が定義されていない。
+    // ② グローバルスコープに定義されている const x7 = 10; を参照する。
+    // 静的（run() 関数が実行されていない時）に名前解決するため、グローバルスコープに定義されている変数を参照している。
+    // 静的スコープの仕組みは function キーワードを使用した関数宣言、メソッド、アロー関数など全ての関数で共通している性質である。
+
+  /* メモリ管理の仕組み
+   ・JavaScript はメモリの解放にガベージコレクションを採用している。
+   ・ガベージコレクションとは...どこからも参照されなくなったデータを不要なデータと判断し、メモリ上から解放する仕組みのこと。 */
+
+    let x8 = "before text";
+    x8 = "after text";
+    // この際に "before text" はどこからも参照されなくなる。
+    // その後、ガベージコレクションによってメモリ上から解放される
+
+    /* ガベレージコレクションと関数
+     ・「関数内で定義したデータ」が解放されるパターンは２パターンある。
+     　・関数が終了した際に解放される場合
+     　・関数が終了しても解放されない場合 */
+
+      /* 関数が終了した際に解放される場合 */
+      function printY() {
+        const y = "y";
+        console.log(y); // "y"
+      }
+      printY();
+      // この時点で "y" は参照されなくり解放される
+
+      /* 関数が終了した際に解放される場合 */
+      function createArray() {
+        const tempArray = [1, 2, 3];
+        return tempArray;
+      }
+      const array = createArray();
+      console.log(array); // [1, 2, 3]
+      // 変数 array が [1, 2, 3] という値を参照しているため解放されない
+
+  /* クロージャーがなぜ動くのか
+    ・「静的スコープ」と「メモリ管理の仕組み」を利用し、関数内から特定の変数を参照し続けることによってクロージャーは成り立っている。
+    */
+    const createCounter = () => {
+      let count = 0;
+      return function increment() {
+        // createCounter() 関数で定義されている count を参照し続ける
+        count = count + 1;
+        return count;
+      };
+    };
+    // counter 変数に increment() 関数が代入される。
+    const counter = createCounter();
+    console.log(counter()) // 1
+    console.log(counter()) // 2
+    // ① counter 変数は increment() 関数を参照している。
+    // ② counter 変数は increment() 関数を経由して count 変数を参照している。
+    // ③ counter 変数を実行後も increment() 関数は参照され続けている。つまり、count の状態も保持されたママになる
+    // counter -> increment -> count
+
+  /* クロージャーの用途
+    ・関数に状態を持たせる手段として
+    ・外から参照できない変数を定義する手段として
+    ・グローバル変数を減らす手段として
+    ・高階関数の一部分として */
+
+    /* グローバル変数を減らす手段 */
+    const createCounter2 = () => {
+      // 外のスコープから`privateCount`を直接参照できない
+      let privateCount = 0;
+      return () => {
+        privateCount++;
+        return `${privateCount}回目`;
+      };
+    };
+    const counter2 = createCounter2();
+    console.log(counter2()); // => "1回目"
+    console.log(counter2()); // => "2回目"
+
+    /* 高階関数を使用することで条件の違う別の関数を定義できる */
+    function greaterThan(n) {
+      return function(m) {
+        return m > n;
+      };
+    }
+    // 5より大きな値かを判定する関数を作成する
+    const greaterThan5 = greaterThan(5);
+    console.log(greaterThan5(4)); // => false
+    console.log(greaterThan5(5)); // => false
+    console.log(greaterThan5(6)); // => true
