ecc507ae7c402b8fe7627a5ead8acda7dd54451e
Hiroya Furusawa <pogbba.6@gmail.com>
2023-03-09 02:12:32 +0900

    マップの反復処理のセクションを追加

--- src/section17_map_and_set/index.js
+++ src/section17_map_and_set/index.js
@@ -33,3 +33,45 @@ console.log(map1.size) // 2
     // 要素を全て削除
     map2.clear();
     console.log(map2.size); // 0
+
+    /*
+        マップの反復処理
+        ・要素を列挙するメソッドとして以下の4つが存在する
+        　・forEach...マップへの挿入順に反復処理をする。`Array.forEach`と似ているが、インデックスの代わりにキーが渡される
+        　・keys...キーを並べた Iterator を返す。Iterator は`for...of`や`Array.from`が使用できる。
+        　・values...`keys`メソッドの key が value にわかった挙動。
+        　・entries...エントリー（`[key, value]`の配列）の Iterator を返す。   */
+    // forEach
+    const map3 = new Map([["key1", "value1"], ["key2", "value2"]]);
+    const result = [];
+    map3.forEach((value, key) => {
+        result.push(`${key}: ${value}`);
+    });
+    console.log(result) // `['key1: value1', 'key2: value2']`
+
+    // keys
+    const map4 = new Map([["key1", "value1"], ["key2", "value2"]]);
+    const result1 = [];
+    for(const key of map4.keys()) {
+        result1.push(key);
+    }
+    console.log(result1); // ['key1', 'key2']
+    // Array を生成
+    const map4KeysArray = Array.from(map4.keys());
+    console.log(map4KeysArray); // ['key1', 'key2']
+
+    // entries
+    const map5 = new Map([["key1", "value1"], ["key2", "value2"]]);
+    const result2 = [];
+    for(const [key, value] of map5.entries()) {
+        result2.push(`${key}: ${value}`);
+    }
+    console.log(result2) // `['key1: value1', 'key2: value2']`
+
+    // map 自身も Iterator を返す。要素はエントリーとなるため、`entries`メソッドを使用した場合と同じ挙動になる
+    const map6 = new Map([["key1", "value1"], ["key2", "value2"]]);
+    const result3 = [];
+    for(const [key, value] of map6) {
+        result3.push(`${key}: ${value}`);
+    }
+    console.log(result3) // `['key1: value1', 'key2: value2']`
