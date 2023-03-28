8cc34dcfb3b8b6453b1001bec37fc9f83c0babb3
Hiroya Furusawa <pogbba.6@gmail.com>
2023-03-29 02:45:49 +0900

    数値を整数にするセクションを追加

--- src/section20_math/index.js
+++ src/section20_math/index.js
@@ -30,3 +30,22 @@ console.log(Math.max(1, 30, 15)); // 30
 const nums2 = [1, 50, 100, 150];
 console.log(Math.min(...nums2)); // 1
 console.log(Math.max(...nums2)); // 150
+
+/*
+    数値を整数にする
+    ・`floor`メソッドは渡した数以下の整数を返す。床関数と呼ばれる。
+    ・`ceil`メソッドは渡した数以上の整数を返す。天井関数と呼ばれる。
+    ・`round`メソッドは渡した数を四捨五入する。
+    ・`trunc`メソッドは渡した数の小数点を切り落とす。正の数の場合は`floor`メソッドと同じ挙動となり、負の数の場合、`ceil`メソッドと同じ挙動となる。  */
+// 床関数
+console.log(Math.floor(1.4)); // 1
+console.log(Math.floor(-1.1)); // -2
+// 天井関数
+console.log(Math.ceil(1.4)); // 2
+console.log(Math.ceil(-1.1)); // -1
+// 四捨五入
+console.log(Math.round(1.4)); // 1
+console.log(Math.round(-1.1)); // -1
+// 小数点を切り落とす
+console.log(Math.trunc(1.8)); // 1
+console.log(Math.trunc(-1.1)); // -1
